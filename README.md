b6f2f8a36e2c6de53963349bd9591baa4023305d
Daniel Shaw <github@dshaw.com>
2010-09-29 22:36:17 -0400

    Handling 'n' connections per client. 'n' currently set to 72 simultaneous connections per client.

---
+++ README.md
@@ -0,0 +1,17 @@
+SocketStuffer
+
+# Objective #
+
+Provide tools to test the performance of Web Socket based applications.
+
+# Project Status #
+
+*NOTE: SocketStuffer is still under development.*
+
+# Dependencies #
+
+npm install node-static websocket-server
+
+# Licence #
+MIT
+
---
+++ server.js
@@ -0,0 +1,57 @@
+var http        = require('http'),
+    sys         = require('sys'),
+    // npm  
+    static      = require('node-static'),
+    ws          = require('websocket-server');
+
+// config
+var fileServer  = new static.Server('./public')
+    port        = parseInt(process.env.PORT) || 8888,
+    debug       = true,
+    connections = {
+      count: 0
+    };
+
+/**
+ * Server
+ */
+var server = ws.createServer({ debug: true }, http.createServer());
+
+server.on("listening", function() {
+  console.log('Server listening on port :' + port);
+  console.log("Listening for web socket connections on port :" + port);
+});
+
+server.on('request', function(request, response) {
+  request.on('end', function() {
+    //if (debug) console.dir(request);
+    fileServer.serve(request, response);
+  });
+}).listen(port);
+
+server.on('connection', function(connection) {
+  console.log(connection.id + ' connected');
+  connections[connection.id] = { messages: []};
+  connections.count++;
+  console.log(connections.length);
+  server.send(connection.id, JSON.stringify({ action: 'welcome'}));
+  server.broadcast(Date.now() + ' ' + connections.count + ' connections');
+
+  connection.on('message', function(message) {
+    connections[connection.id].messages.push(message);
+    try {
+      var msg = JSON.parse(message);
+      if (msg.type === 'click') {
+        msg.type = 'confirm';
+      }
+      server.send(connection.id, JSON.stringify(msg));
+    } catch (e) {}
+  });
+});
+
+server.on('close', function(connection) {
+  console.log(connection.id + ' closed');
+  delete connections[connection.id];
+  connections.count--;
+  console.log(connections.length)
+});
